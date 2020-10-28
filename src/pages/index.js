d1e52cb1db94ea74f15ad1ec56b60b41d9daadc1
Knut Bjørnevik <kbjornevik@hotmail.com>
2020-10-28 18:25:57 +0100

    Fjernet heading

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,8 +8,7 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
         <SEO title="Home" />
-    <h1>Strom</h1>
-        <iframe width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiN2Q5M2M0ZTctNzMzZS00ZjliLWJlM2YtZmU5NDk5N2U1ZGFkIiwidCI6ImNiMjQwMDExLTg5MTYtNGFjNi05ZTU5LWU1NGMwY2IxNGRkZCIsImMiOjh9&pageName=ReportSection1a134217d3b62c57a41c" frameborder="0" allowFullScreen="true">
+       <iframe width="800" height="573.5" src="https://app.powerbi.com/view?r=eyJrIjoiN2Q5M2M0ZTctNzMzZS00ZjliLWJlM2YtZmU5NDk5N2U1ZGFkIiwidCI6ImNiMjQwMDExLTg5MTYtNGFjNi05ZTU5LWU1NGMwY2IxNGRkZCIsImMiOjh9&pageName=ReportSection1a134217d3b62c57a41c" frameborder="0" allowFullScreen="true">
         </iframe>
    
     <p>Now go build something great.</p>
