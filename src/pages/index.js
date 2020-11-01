3382ae9133fe7ed525d90d4dc0c25c41f8174966
Knut Bjørnevik <kbjornevik@hotmail.com>
2020-11-01 07:32:56 +0100

    Fjernet bilde

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,11 +10,7 @@ const IndexPage = () => (
         <SEO title="Home" />
        <iframe width="800" height="573.5" src="https://app.powerbi.com/view?r=eyJrIjoiN2Q5M2M0ZTctNzMzZS00ZjliLWJlM2YtZmU5NDk5N2U1ZGFkIiwidCI6ImNiMjQwMDExLTg5MTYtNGFjNi05ZTU5LWU1NGMwY2IxNGRkZCIsImMiOjh9&pageName=ReportSection1a134217d3b62c57a41c" frameborder="0" allowFullScreen="true">
         </iframe>
-   
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
+  
     <Link to="/page-2/">Go to page 2</Link> <br />
     <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
   </Layout>
