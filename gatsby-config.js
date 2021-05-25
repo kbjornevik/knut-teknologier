6e5707e3cdb6d7c903c244c74298128169d65e87
Knut Bjørnevik <kbjornevik@hotmail.com>
2021-05-25 13:38:29 +0200

    Sitemap
    Sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,10 +4,12 @@ module.exports = {
     description: ` Knut Bjørnevik med ulike tema som intresserer ham, Filefjell, Sykkel, IT-Teknologi`,
     author: `kbjornevik@hotmail.com`,
     keywords: `Ut på Tur, Strava, Sykkel, Fjell,Filefjell`,
+    siteUrl: `https://knutteknologier.gtsb.io/`,
 
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    'gatsby-plugin-sitemap',
     `gatsby-plugin-sass`,
     {
       resolve: `gatsby-source-filesystem`,
@@ -60,33 +62,6 @@ module.exports = {
        },
     },
   
-    {
-      resolve: `gatsby-plugin-google-gtag`,
-      options: {
-        // You can add multiple tracking ids and a pageview event will be fired for all of them.
-        trackingIds: [
-
-          "GA-TRACKING_ID", // Google Analytics / GA
-          "AW-CONVERSION_ID", // Google Ads / Adwords / AW 
-          "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
-        ],
-        // This object gets passed directly to the gtag config command
-        // This config will be shared across all trackingIds
-        gtagConfig: {
-          optimize_id: "OPT_CONTAINER_ID",
-          anonymize_ip: true,
-          cookie_expires: 0,
-        },
-        // This object is used for configuration specific to this plugin
-        pluginConfig: {
-          // Puts tracking script in the head instead of the body
-          head: false,
-          // Setting this parameter is also optional
-          respectDNT: true,
-          // Avoids sending pageview hits from custom paths
-          exclude: ["/preview/**", "/do-not-track/me/too/"],
-        },
-      },
-    },
+ 
   ],
 }
