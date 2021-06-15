8c7fd0ac6ca543e2d8da615525add4791509f584
Knut Bjørnevik <kbjornevik@hotmail.com>
2021-06-15 15:02:02 +0200

    API File update

--- src/components/nordpoolpris.js
+++ src/components/nordpoolpris.js
@@ -3,14 +3,6 @@ import { render } from 'react-dom';
 import { Tooltip,Legend,LineChart, Line, XAxis, YAxis,CartesianGrid } from 'recharts';
 const styles = { fontFamily: 'sans - serif', textAlign: 'center'};
 const linedata = [
- { 'Kr_sand':0.56,
-   'Bergen':0.50,
-   'Oslo':0.50,
-   'Tromsø':0.24,
-   'Molde':0.45,
-   'Tr_heim':0.45,
-   'Dato':'06-01'}
-,
  { 'Kr_sand':0.55,
    'Bergen':0.50,
    'Oslo':0.50,
@@ -122,6 +114,14 @@ const linedata = [
    'Molde':0.21,
    'Tr_heim':0.21,
    'Dato':'06-15'}
+,
+ { 'Kr_sand':0.51,
+   'Bergen':0.46,
+   'Oslo':0.46,
+   'Tromsø':0.22,
+   'Molde':0.33,
+   'Tr_heim':0.33,
+   'Dato':'06-16'}
  ] 
     const LineChartPris = ({ }) =>
     {   return ( < LineChart width ={ 700}   height ={ 250} data ={ linedata} 
